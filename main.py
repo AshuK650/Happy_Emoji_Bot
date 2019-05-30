fca9d70fac1bc3a537b955fcebbf83fce809953a
Ashu Kher <ashukher1@gmail.com>
2019-05-30 11:29:30 -0400

    changed ordering and spacing

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import credentials as c
 import tweet
 
 
+credentials = c.get_credentials()
+
 #interval_int = 60 * 60 * 4  # 4 hours
-interval_int = 60 * 15  # 15 minutes 
-
-credentials = c.get_credentials() 
-
+interval_int = 60 * 15  # 15 minutes  
 
 # Twitter authentication set up goes here 
+
 #auth = tweetpy.OAuthHandler(credentials["CONSUMER_KEY"], credentials["CONSUMER_SECRET"])
 #auth.set_access_token(credentials["ACCESS_KEY"], credentials["ACCESS_SECRET"])
 #api = tweepy.API(auth)
