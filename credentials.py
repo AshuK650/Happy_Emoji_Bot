a206b368094ae2d344351f8ea51bd7b82b927b73
Ashu Kher <ashukher1@gmail.com>
2019-05-30 11:16:29 -0400

    Updated files

---
+++ credentials.py
@@ -0,0 +1,17 @@
+def get_credentials(): 
+    
+    file_obj = open("credentials.txt") 
+    
+    credentials_dict = {} 
+    
+    for line in file_obj: 
+        line = line.split(":") 
+        
+        credentials_dict[line[0].strip()] = line[1].strip() 
+    
+    return credentials_dict
+    
+    pass
+
+if __name__ == "__main__": 
+    get_credentials() 
