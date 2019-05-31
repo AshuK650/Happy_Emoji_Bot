dcd3ee25c4e08430ffb7c4aeb38101c2aea9f55c
Ashu Kher <ashukher1@gmail.com>
2019-05-31 15:25:02 -0400

    changed back to once a day

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import credentials as c 
-import datetime as dt
-import time as t 
 import tweepy 
 import tweet
 
@@ -10,19 +8,11 @@ credentials = c.get_credentials()
 auth = tweepy.OAuthHandler(credentials["CONSUMER_KEY"], credentials["CONSUMER_SECRET"])
 auth.set_access_token(credentials["ACCESS_KEY"], credentials["ACCESS_SECRET"])
 api = tweepy.API(auth)
-
-while True: 
-    now_obj = dt.datetime.now() 
+      
+try: 
+    tweet_str = tweet.get_tweet() 
+    api.update_status(tweet_str)
     
-    if now_obj.hour % 4 == 0 and now_obj.minute == 0: 
-        
-        try: 
-            tweet_str = tweet.get_tweet() 
-            api.update_status(tweet_str)
-            
-            t.sleep(60)
-        except TweepError: 
-            api.update_status("Sorry! Check again tomorrow \U0001f600")
-            
-            t.sleep(60)
-            
+except TweepError: 
+    api.update_status("Sorry! Check again tomorrow \U0001f600")
+    
