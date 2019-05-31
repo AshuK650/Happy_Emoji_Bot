8427852e8aeaea8af7d8ca820a3194640e8621fb
Ashu Kher <ashukher1@gmail.com>
2019-05-31 14:23:13 -0400

    changed how often it would tweet

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import time as t 
 import credentials as c
+import datetime as dt 
 import tweepy 
 import tweet
 
 
 credentials = c.get_credentials()
-interval_int = 60 * 60 * 4  # 4 hours
 
 
 # Twitter authentication set up goes here 
@@ -16,8 +15,12 @@ api = tweepy.API(auth)
 
 # loop to tweet ever 4 hours 
 while True: 
-    tweet_str = tweet.get_tweet() 
     
-    api.update_status(tweet_str)
+    now_obj = dt.datetime.now() 
     
-    t.sleep(interval_int) 
+    if now_obj.hour % 6 == 0 and now_obj.hour != 0:  # tweet ever 4 hours 
+        
+        tweet_str = tweet.get_tweet() 
+        
+        api.update_status(tweet_str)
+        
