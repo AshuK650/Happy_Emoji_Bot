573a940b2020f327b3fe04c071bfe568552c4609
Ashu Kher <ashukher1@gmail.com>
2019-05-31 14:59:53 -0400

    changed it to tweet only once
    will use schedueled tasks from pythonanywhere to tweet once a day

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
-import credentials as c
-import datetime as dt 
-import time as t 
+import credentials as c 
 import tweepy 
 import tweet
 
 
 credentials = c.get_credentials()
 
-
-# Twitter authentication set up goes here 
 auth = tweepy.OAuthHandler(credentials["CONSUMER_KEY"], credentials["CONSUMER_SECRET"])
 auth.set_access_token(credentials["ACCESS_KEY"], credentials["ACCESS_SECRET"])
 api = tweepy.API(auth)
-
-
-# loop to tweet ever 4 hours 
-while True: 
+      
+try: 
+    tweet_str = tweet.get_tweet() 
+    api.update_status(tweet_str)
     
-    now_obj = dt.datetime.now() 
+except TweepError: 
+    api.update_status("Sorry! Check again tomorrow \U0001f600")
     
-    if now_obj.hour % 4 == 0 and now_obj.minute == 0:  # tweet ever 4 hours 
-        
-        try: 
-            tweet_str = tweet.get_tweet() 
-            api.update_status(tweet_str)
-            print(tweet_str)
-            print(len(tweet_str))
-            t.sleep(60)
-            
-        except TweepError: 
-            api.update_status("Check Again Next Time \U0001f600")
-            t.sleep(60)
-            
-            pass
-        
