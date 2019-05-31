42a82f5b875e3b133cc2f046efd0490d2c85ce13
Ashu Kher <ashukher1@gmail.com>
2019-05-31 15:11:29 -0400

    set up with heroku

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import credentials as c 
+import datetime as dt
+import time as t 
 import tweepy 
 import tweet
 
@@ -8,11 +10,19 @@ credentials = c.get_credentials()
 auth = tweepy.OAuthHandler(credentials["CONSUMER_KEY"], credentials["CONSUMER_SECRET"])
 auth.set_access_token(credentials["ACCESS_KEY"], credentials["ACCESS_SECRET"])
 api = tweepy.API(auth)
-      
-try: 
-    tweet_str = tweet.get_tweet() 
-    api.update_status(tweet_str)
+
+while True: 
+    now_obj = dt.datetime.now() 
     
-except TweepError: 
-    api.update_status("Sorry! Check again tomorrow \U0001f600")
-    
+    if now_obj.hour % 4 == 0 and now_obj.minute == 0: 
+        
+        try: 
+            tweet_str = tweet.get_tweet() 
+            api.update_status(tweet_str)
+            
+            t.sleep(60)
+        except TweepError: 
+            api.update_status("Sorry! Check again tomorrow \U0001f600")
+            
+            t.sleep(60)
+            
