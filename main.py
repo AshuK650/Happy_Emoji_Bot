b3a929d7f3e6674632ea94361dfcb72b94c44f6e
Ashu Kher <ashukher1@gmail.com>
2019-06-01 15:44:27 -0400

    added time tracking for bash console in pythonanywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import credentials as c 
 import time as t 
+import datetime as dt 
 import tweepy 
 import tweet
 
@@ -11,7 +12,14 @@ auth.set_access_token(credentials["ACCESS_KEY"], credentials["ACCESS_SECRET"])
 api = tweepy.API(auth)
 
 while True: 
-        
+    
+    now_obj = dt.datetime.now() 
+    
+    hour_int = now_obj.hour + 4
+    min_int = now_obj.minute
+    sec_int = now_obj.second
+    
+    
     try: 
         tweet_str = tweet.get_tweet() 
         api.update_status(tweet_str)
@@ -19,8 +27,20 @@ while True:
         # used for the bash console in pythonanywhere
         print(tweet_str)
         print(len(tweet_str))
-    except TweepError: 
+        
+        if hour_int >= 12: 
+            print("Next tweet at: {}:{}:{} PM".format(hour_int - 12, min_int, sec_int))
+        else: 
+            print("Next tweet at: {}:{}:{} PM".format(hour_int, min_int, sec_int))
+
+        
+    except: 
         api.update_status("Sorry! Check again later \U0001f600")
         print("Sorry! Check again later \U0001f600")
+        
+        if hour_int >= 12: 
+            print("Next tweet at: {}:{}:{} PM".format(hour_int - 12, min_int, sec_int))
+        else: 
+            print("Next tweet at: {}:{}:{} PM".format(hour_int, min_int, sec_int))
         
     t.sleep(interval_int) 
