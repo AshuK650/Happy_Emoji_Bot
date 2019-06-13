32005e8da87a52f1f887d0ca26680aa6b12e77bf
Ashu Kher <ashukher1@gmail.com>
2019-06-13 10:14:54 -0400

    included 0 in random times generated

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ auth.set_access_token(c["ACCESS_KEY"], c["ACCESS_SECRET"])
 api = tweepy.API(auth)
 
 while True:
-    rand_hour = r.randint(1,4) 
-    rand_min = r.randint(1,60) 
-    rand_sec = r.randint(1,60) 
+    rand_hour = r.randint(0,4) 
+    rand_min = r.randint(0,60) 
+    rand_sec = r.randint(0,60) 
     
     interval_int = (rand_hour * 3600)  + (rand_min * 60) + rand_sec
     
