0d90111d0aae4f505f1e7da276d55c4b11ab4b78
Ashu Kher <ashukher1@gmail.com>
2019-06-14 13:02:03 -0400

    changed to be single tweet

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import credentials, tweepy, tweet, time, random 
+import credentials, tweepy, tweet
 
 
 c = credentials.get_credentials()
@@ -7,23 +7,13 @@ auth = tweepy.OAuthHandler(c["CONSUMER_KEY"], c["CONSUMER_SECRET"])
 auth.set_access_token(c["ACCESS_KEY"], c["ACCESS_SECRET"])
 api = tweepy.API(auth)
 
-while True:
-    rand_hour = random.randint(0,4) 
-    rand_min = random.randint(0,60) 
-    rand_sec = random.randint(0,60) 
+try: 
+    tweet_str = tweet.get_tweet() 
+    api.update_status(tweet_str)
     
-    interval_int = (rand_hour * 3600)  + (rand_min * 60) + rand_sec
-    
-    try: 
-        tweet_str = tweet.get_tweet() 
-        api.update_status(tweet_str)
-        
-        print(tweet_str)
-        print(str(len(tweet_str)) + "\n")
-        print("Next tweet in: {} hours, {} minutes, and {} seconds".format(rand_hour, rand_min, rand_sec))       
-                
-    except (Exception, tweepy.TweepError): 
-        print("Exception Occured. Just wait for the next tweet ")
-        print("Next tweet in: {} hours, {} minutes, and {} seconds".format(rand_hour, rand_min, rand_sec))
-    
-    time.sleep(interval_int) 
+    print(tweet_str)
+    print(str(len(tweet_str)) + "\n")
+            
+except (Exception, tweepy.TweepError): 
+    print("Exception Occured. Just wait for the next tweet ")
+
